772e0318de0dfb62e3be22a9aff93369c0f23af5
Joel Nothman <joel.nothman@gmail.com>
2017-11-15 18:13:59 +1100

    Print record count

--- scraper.py
+++ scraper.py
@@ -7,5 +7,6 @@ df.todate = df.todate.astype(str)
 df.fromdate = df.fromdate.astype(str)
 df = df.fillna('')
 df['office_id_number'] = df.office_id.map(lambda s: int(s.rpartition('/')[-1]))
+print('Got', len(df), 'records')
 scraperwiki.sqlite.save(unique_keys=['office_id_number'],
                         data=df.to_dict(orient='records'))
