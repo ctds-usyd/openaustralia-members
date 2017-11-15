bd909bbaa2cd26073aa51f3ab03e7cb17eb03276
Joel Nothman <joel.nothman@gmail.com>
2017-11-15 18:06:19 +1100

    Try avoid invalid data types for sqlite

--- scraper.py
+++ scraper.py
@@ -3,5 +3,8 @@ import scraperwiki
 
 
 df = openaustraliamembers.OpenAustraliaMembers().offices().reset_index()
+df = df.fillna(None)
+df.todate = df.todate.astype(str)
+df.fromdate = df.fromdate.astype(str)
 scraperwiki.sqlite.save(unique_keys=['office_id'],
                         data=df.to_dict(orient='records'))
