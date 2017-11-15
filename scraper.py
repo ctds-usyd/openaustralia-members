04940cd6eb601334b216ab5592c0d5e8bc567451
Joel Nothman <joel.nothman@gmail.com>
2017-11-15 18:16:02 +1100

    arbitrary select

--- scraper.py
+++ scraper.py
@@ -10,3 +10,4 @@ df['office_id_number'] = df.office_id.map(lambda s: int(s.rpartition('/')[-1]))
 print('Got', len(df), 'records')
 scraperwiki.sqlite.save(unique_keys=['office_id_number'],
                         data=df.to_dict(orient='records'))
+scraperwiki.sql.select("* from data limit 10")
