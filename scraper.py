7609f3aa45ce9384f60c3193b2f37f61bd05aae5
Joel Nothman <joel.nothman@gmail.com>
2017-11-15 18:10:35 +1100

    Fill NaN as empty string

--- scraper.py
+++ scraper.py
@@ -5,6 +5,7 @@ import scraperwiki
 df = openaustraliamembers.OpenAustraliaMembers().offices().reset_index()
 df.todate = df.todate.astype(str)
 df.fromdate = df.fromdate.astype(str)
+df.fillna('')
 df['office_id_number'] = df.office_id.map(lambda s: int(s.rpartition('/')[-1]))
 scraperwiki.sqlite.save(unique_keys=['office_id_number'],
                         data=df.to_dict(orient='records'))
